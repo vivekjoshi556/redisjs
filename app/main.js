47dcfaa0276015d436c8f8a3e401c2c4d90b200d
vivekjoshi556 <vivekjoshi556@gmail.com>
2024-04-26 22:07:33 +0530

    feat: handling multiple pings

--- app/main.js
+++ app/main.js
@@ -2,7 +2,15 @@ const net = require("net");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-	connection.write("+PONG\r\n");
+	console.log("Server Listening at: 127.0.0.1:6379");
+
+	connection.on("data", () => {
+		console.log("Got Data");
+		connection.write("+PONG\r\n");
+	});
+	connection.on("close", () => {
+		console.log("Closing connection");
+	});
 });
 
 server.listen(6379, "127.0.0.1");

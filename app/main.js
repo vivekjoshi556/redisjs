e5280e44bb4569ec19e3b2fcb8e8dfa69934dc85
vivekjoshi556 <vivekjoshi556@gmail.com>
2024-04-26 17:59:48 +0530

    feat: ping response

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const net = require("net");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-	// Handle connection
+	connection.write("+PONG\r\n");
 });
 
 server.listen(6379, "127.0.0.1");

b7afdd5da7885cbe311ee915da8099c35d4a7361
vivekjoshi556 <vivekjoshi556@gmail.com>
2024-05-05 22:42:39 +0530

    codecrafters submit [skip ci]

--- app/commands/Get.js
+++ app/commands/Get.js
@@ -6,12 +6,16 @@ module.exports = class Get {
     if(commands.length < 2) 
       throw new Error("Not enough arguments");
 
-    let store = new Store({});
-    if(store.data[commands[1]] === undefined) {
+    let store = new Store();
+    let key = commands[1];
+    
+    // If the key is not present or,
+    // If the key is invalid now and hasn't been deleted yet.
+    if(store.data[key] === undefined || (store.data[key].expiry && store.data[key].expiry <= new Date().getTime())) {
       return new BulkStringParser().serialize(null);
     }
 
     let parser = new BulkStringParser();
-    return parser.serialize(store.data[commands[1]]);
+    return parser.serialize(store.data[commands[1]].value);
   }
 }
--- app/commands/Set.js
+++ app/commands/Set.js
@@ -3,12 +3,32 @@ const { StringParser } = require("../parser/");
 
 module.exports = class Set { 
   execute(commands) {
-    if(commands.length < 3) 
+    if(commands.length < 3) {
       throw new Error("Not enough arguments");
+    }
 
-    let store = new Store({});
-    store.data[commands[1]] = commands[2];
+    let [_, key, value, __ , expiry] = commands;
+
+    let store = new Store();
+    store.data[key] = {value}
+
+    if(expiry) {
+      const time = new Date().getTime();
+      let expiryTime = time + parseInt(expiry);
+      
+      store.data[key].expiry = expiryTime;
+
+      setTimeout(() => {
+        this.delete(key);
+      }, parseInt(expiry));
+    }
+
     let parser = new StringParser();
     return parser.serialize("OK");
+  }
+
+  delete(key) {
+    let store = new Store();
+    delete store.data[key];
   }
 }

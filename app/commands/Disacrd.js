0993d9730bf4c6d43f1da6a450dc5eca8e5a5766
Vivek Joshi <37882929+vivekjoshi556@users.noreply.github.com>
2024-11-17 20:07:35 +0000

    codecrafters submit [skip ci]

--- app/commands/Disacrd.js
+++ app/commands/Disacrd.js
@@ -1,10 +1,12 @@
-const { StringParser } = require("../parser/");
+const { StringParser, ErrParser } = require("../parser/");
 
 module.exports = class Del { 
   constructor({connection}) {
     this.connection = connection;
   }
   execute(commands) {
+    if(!this.connection['transaction'])
+      return new ErrParser().serialize("ERR", "DISCARD without MULTI");
 
     this.connection['transaction'] = false;
     this.connection['transactionQueue'] = [];
